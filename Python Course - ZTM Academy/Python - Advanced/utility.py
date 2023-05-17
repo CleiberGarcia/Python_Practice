28bed7d6b6f21e04821e33087295406873b9c267
Cleiber Garcia <cleiber.negocios@gmail.com>
2023-05-17 08:06:46 -0300

    Add files via upload

---
+++ Python Course - ZTM Academy/Python - Advanced/utility.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 14 04:04:04 2022
+
+@author: Cleiber Garcia
+"""
+
+# utility.py
+
+def multiply(num1,num2):
+    return num1*num2
+
+def divide(num1,num2):
+    return num1/num2
